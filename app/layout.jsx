3c859af292bfccefba6744e7e0182f5f490daa9e
Mohammad Ali <alikhanak012345@gmail.com>
2026-07-31 00:30:29 +0500

    google search console verification meta tag added

--- app/layout.jsx
+++ app/layout.jsx
@@ -17,9 +17,12 @@ export default function RootLayout({ children }) {
         <meta
           name="description"
           content="Shop 100% HD Swiss lace human hair wigs, body wave, deep wave, and luxury bundle deals from VERSATILE BY VERSHA' — One Woman. Every Look."
-          
-          
         />
+        <meta
+          name="google-site-verification"
+          content="mb4aMdCt2RYB5vQetR1aqxXwAlHR8qG21VmNkahcwC8"
+        />
+
         <link rel="icon" href="/images/logo.png" />
       </head>
       <body className="flex flex-col min-h-screen">
